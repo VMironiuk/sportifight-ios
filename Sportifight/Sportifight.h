67efda0e3a72e2c08920c8fb9e49f697597b1975
Vladimir Mironjuk <vladimironiuk@gmail.com>
2022-01-22 17:11:13 +0200

    Initial commit

---
+++ Sportifight/Sportifight.h
@@ -0,0 +1,18 @@
+//
+//  Sportifight.h
+//  Sportifight
+//
+//  Created by Vladimir Mironiuk on 22.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sportifight.
+FOUNDATION_EXPORT double SportifightVersionNumber;
+
+//! Project version string for Sportifight.
+FOUNDATION_EXPORT const unsigned char SportifightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sportifight/PublicHeader.h>
+
+
